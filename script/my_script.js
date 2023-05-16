50c7f6375894c0b3ba50536d3f9704d02b7367c0
EmanuelaPau <uapame@gmail.com>
2023-05-16 15:19:22 +0200

    Start Bonus 1: Add container

--- script/my_script.js
+++ script/my_script.js
@@ -1,15 +1,3 @@
-// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-
-// Come faccio a sapere se un numero è divisibile per un altro ?
-//     Abbiamo visto qualcosa di particolare che possiamo usare ?
-
-//         Consigli del giorno:
-// Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-
 // Problem 
 console.log('test');
 // Add 1 to 100 count
@@ -27,4 +15,6 @@ for (let count = 1; count <= 100; count++) {
     }
 }
 
+// BONUS 1:
+// Crea un container nel DOM, aggiungendo(attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 
